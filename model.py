24cc9088c415a8d8fbb2fb975bf78119232b9628
Thoalfeqar-gata <thoalfeqarkatajarullah@gmail.com>
2026-06-26 19:04:25 +0300

    finished pretraining (initial pretraining)

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class MLPDecoder(nn.Module):
-    def __init__(self, channel_dims = [96, 192, 384, 768], hidden_dim = 256):
+    def __init__(self, channel_dims = [96, 192, 384, 768], hidden_dim = 256, out_channels = 1):
         super(MLPDecoder, self).__init__()
         self.conv1 = nn.Conv2d(in_channels = channel_dims[0], out_channels = hidden_dim, kernel_size = 1, stride = 1, padding = 0)
         self.conv2 = nn.Conv2d(in_channels = channel_dims[1], out_channels = hidden_dim, kernel_size = 1, stride = 1, padding = 0)
@@ -15,7 +15,7 @@ class MLPDecoder(nn.Module):
             nn.Conv2d(in_channels = 4 * hidden_dim, out_channels = hidden_dim, kernel_size = 1, stride = 1, padding = 0),
             nn.BatchNorm2d(num_features = hidden_dim),
             nn.ReLU(inplace = True),
-            nn.Conv2d(in_channels = hidden_dim, out_channels = 1, kernel_size = 1, stride = 1, padding = 0)
+            nn.Conv2d(in_channels = hidden_dim, out_channels = out_channels, kernel_size = 1, stride = 1, padding = 0)
         )
         
     def forward(self, multiscale_features):
@@ -36,7 +36,7 @@ class MLPDecoder(nn.Module):
     
 
 class BrainTumorModel(nn.Module):
-    def __init__(self, num_classes = 4, dropout_ratio = 0.1):
+    def __init__(self, num_classes = 4, out_channels = 1, dropout_ratio = 0.1):
         super(BrainTumorModel, self).__init__()
         # Backbone with features_only=True to extract multiscale intermediate features
         self.backbone = timm.create_model('davit_tiny.msft_in1k', pretrained = True, features_only = True)
@@ -45,8 +45,8 @@ class BrainTumorModel(nn.Module):
         channel_dims = self.backbone.feature_info.channels()
         num_features = channel_dims[-1]
         
-        # Segmentation decoder
-        self.decoder = MLPDecoder(channel_dims = channel_dims, hidden_dim = 256)
+        # Segmentation decoder (configured with out_channels parameter)
+        self.decoder = MLPDecoder(channel_dims = channel_dims, hidden_dim = 256, out_channels = out_channels)
         
         # Classification head (comparable to native DaViT head structure)
         self.global_pool = nn.AdaptiveAvgPool2d(1)
@@ -59,9 +59,9 @@ class BrainTumorModel(nn.Module):
         # Expected outputs: [B, 96, 56, 56], [B, 192, 28, 28], [B, 384, 14, 14], [B, 768, 7, 7]
         features = self.backbone(x)
         
-        # 1. Segmentation Output
+        # 1. Segmentation / Reconstruction Output
         seg_out = self.decoder(features)
-        # Upsample segmentation logits to original input resolution
+        # Upsample output logits to original input resolution
         seg_out = F.interpolate(seg_out, size = x.shape[2:], mode = 'bilinear', align_corners = False)
         
         # 2. Classification Output
--- utils.py
+++ utils.py
@@ -1,5 +1,43 @@
+import torch
+import torchvision
 import cv2, numpy as np
+from torch.utils.data import Dataset
 from PIL import Image
+
+
+class PretrainingDataset(torchvision.datasets.ImageFolder):
+    def __init__(self, root, geom_transform=None, photo_transform=None, base_transform=None):
+        super(PretrainingDataset, self).__init__(root=root)
+        self.geom_transform = geom_transform
+        self.photo_transform = photo_transform
+        self.base_transform = base_transform # For ToTensor and Normalization
+
+    def __getitem__(self, index):
+        path, label = self.samples[index]
+        img = self.loader(path)
+        
+        # 1. Apply geometric transforms to get the clean PIL image (spatially aligned target)
+        if self.geom_transform is not None:
+            clean_pil = self.geom_transform(img)
+        else:
+            clean_pil = img
+            
+        # 2. Apply photometric corruptions to get the augmented PIL image (model input)
+        if self.photo_transform is not None:
+            augmented_pil = self.photo_transform(clean_pil)
+        else:
+            augmented_pil = clean_pil
+            
+        # 3. Convert both PIL images to normalized PyTorch tensors
+        if self.base_transform is not None:
+            augmented_img = self.base_transform(augmented_pil)
+            clean_img = self.base_transform(clean_pil)
+        else:
+            augmented_img = augmented_pil
+            clean_img = clean_pil
+            
+        return augmented_img, clean_img, label
+
 
 class CLAHETransform:
     def __init__(self, clipLimit=2.0, tileGridSize=(8,8)):
